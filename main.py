6e66235b10e09fff0aae67dce74c7a76757b24ea
Mohammad Rehan Tabish Shekh Ruknoddin <mdrehanshaikh39@gmail.com>
2026-08-02 23:33:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from mean_var_std import calculate
+
+print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
+print(calculate([2, 6, 2, 8, 4, 0, 1, 5, 7]))
+
+# Uncomment the line below to test the ValueError
+# print(calculate([1, 2, 3]))
